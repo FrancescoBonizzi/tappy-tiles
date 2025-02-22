c1a162a28a5bc28a67e34c061081df122ec53966
Francesco Bonizzi <f.bonizzi@outlook.it>
2025-02-23 00:13:56 +0100

    refactor: enhance Game and Menu components with improved animations and layout

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,5 +1,11 @@
 function Game() {
-    return <div>Game</div>;
+    return <div
+        className="flex h-screen bg-gray-900 text-white flex-col items-center justify-center">
+
+        <p className={"font-playful text-4xl mt-8"}>
+            Game
+        </p>
+    </div>;
 }
 
 export default Game;
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,27 +1,38 @@
 import {motion} from "motion/react"
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 function Menu() {
     const navigate = useNavigate();
+    const [isFading, setIsFading] = useState(false);
+    const animationDurationMs = isFading ? 1000 : 1500;
+    const repeat = isFading ? 0 : Infinity;
+
+    const handleClick = () => {
+        setIsFading(true);
+        setTimeout(() => navigate("/game"), animationDurationMs);
+    };
 
     return (
-        <motion.div
-            onClick={() => navigate("/game")}
-            className="cursor-pointer flex flex-col items-center justify-center h-screen bg-gray-900 text-white text-center"
-            initial={{scale: 0.8}}
-            animate={{scale: [1, 1.1, 1]}}
-            transition={{duration: 1.5, ease: "easeInOut", repeat: Infinity}}>
+        <div
+            onClick={handleClick}
+            className="cursor-pointer h-screen flex flex-col items-center justify-center bg-gray-900 text-white text-center">
+            <motion.div
+                className="flex flex-col items-center justify-center"
+                animate={{scale: [1, 1.1, 1], opacity: isFading ? 0 : 1}}
+                transition={{duration: animationDurationMs / 1000, ease: "easeInOut", repeat: repeat}}>
 
-            <img src="/tappy-tiles-logo.png"
-                 alt="TappyTiles Logo"
-                 className="w-52 rounded-xl"
-            />
+                <img src="/tappy-tiles-logo.png"
+                     alt="TappyTiles Logo"
+                     className="w-52 rounded-xl"
+                />
 
-            <p className="font-playful text-4xl mt-8">
-                Tocca per giocare!
-            </p>
+                <p className="font-playful text-4xl mt-8">
+                    Tocca per giocare!
+                </p>
 
-        </motion.div>
+            </motion.div>
+        </div>
     );
 }
 
