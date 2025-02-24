48ae5ac330c591dcedef605cfda2aeead04f88ea
Francesco Bonizzi <f.bonizzi@outlook.it>
2025-02-25 00:50:19 +0100

    feat: improve styling of RestartButton and Game Over scene for better responsiveness and visual appeal

--- src/scenes/scene4GameOver.tsx
+++ src/scenes/scene4GameOver.tsx
@@ -4,13 +4,13 @@ import RestartButton from "../components/RestartButton.tsx";
 function Scene4GameOver() {
     return (
         <motion.div
-            className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-75 text-white"
+            className="flex flex-col items-center h-screen justify-center bg-black bg-opacity-75 text-white"
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             transition={{duration: 0.5}}>
 
             <motion.p
-                className="text-5xl font-bold mb-8 font-playful"
+                className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-8 font-playful text-center"
                 initial={{scale: 0.5}}
                 animate={{scale: 1.2}}
                 transition={{duration: 0.5, yoyo: Infinity}}>
