07f0d64dd6bd01e544bbb880fab2c91f166c70ed
Francesco Bonizzi <f.bonizzi@outlook.it>
2025-02-25 00:05:27 +0100

    feat: enhance animation effects for RestartButton component

--- src/components/RestartButton.tsx
+++ src/components/RestartButton.tsx
@@ -9,11 +9,13 @@ const RestartButton = () => {
     return (
         <motion.button
             className="mt-4 sm:p-2 md:p-4 lg:p-6 bg-white cursor-pointer text-black rounded-full shadow-lg lg:text-6xl md:text-4xl sm:text-2xl font-bold flex items-center justify-center"
-            initial={{opacity: 0, scale: 0.5, rotate: -90}}
-            animate={{opacity: 1, scale: 1, rotate: 0}}
-            whileHover={{scale: 1.1, rotate: 10}}
-            whileTap={{scale: 0.9, rotate: -10}}
-            transition={{duration: 0.5}}
+            initial={{scale: 0.5}}
+            animate={{
+                opacity: 1.5,
+                scale: [1, 1.05, 1],
+                rotate: [0, 5, -5, 0]
+            }}
+            transition={{duration: 1, repeat: Infinity, ease: "easeInOut"}} // Ciclo infinito
             onClick={() => navigate('/choose-color')}
         >
             <FaRedo/>
