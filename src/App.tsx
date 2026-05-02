4c488c0358c7a64e2e6bac10bde68a7d3529e4fb
Francesco Bonizzi <f.bonizzi@outlook.it>
2026-05-02 15:39:03 +0200

    refactor: reorder import statements and clean up formatting in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Scene1StartingMenu from "./scenes/scene1StartingMenu.tsx";
 import Scene2ColorSelector from "./scenes/scene2ColorSelector.tsx";
 import Scene3Game from "./scenes/scene3Game.tsx";
@@ -6,20 +6,19 @@ import Scene4GameOver from "./scenes/scene4GameOver.tsx";
 import Scene5Win from "./scenes/scene5Win.tsx";
 
 function App() {
+  const basename = import.meta.env.BASE_URL.replace(/\/+$/, "") || "/";
 
-    const basename = import.meta.env.BASE_URL; // Imposta automaticamente il base URL in base al deploy
-
-    return (
-        <BrowserRouter basename={basename}>
-            <Routes>
-                <Route path="/" element={<Scene1StartingMenu />} />
-                <Route path="/choose-color" element={<Scene2ColorSelector />} />
-                <Route path="/game" element={<Scene3Game />} />
-                <Route path={"/game-over"} element={<Scene4GameOver />} />
-                <Route path={"/win"} element={<Scene5Win />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter basename={basename}>
+      <Routes>
+        <Route path="/" element={<Scene1StartingMenu />} />
+        <Route path="/choose-color" element={<Scene2ColorSelector />} />
+        <Route path="/game" element={<Scene3Game />} />
+        <Route path={"/game-over"} element={<Scene4GameOver />} />
+        <Route path={"/win"} element={<Scene5Win />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
